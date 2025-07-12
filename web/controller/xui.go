79221d42bcf0e0afc2fc41a664ddf036127af49c
心隨緣動 <88259403+xeefei@users.noreply.github.com>
2025-07-13 00:23:52 +0800

    v2.6.2
    v2.6.2

--- web/controller/xui.go
+++ web/controller/xui.go
@@ -26,6 +26,7 @@ func (a *XUIController) initRouter(g *gin.RouterGroup) {
 	g.GET("/inbounds", a.inbounds)
 	g.GET("/settings", a.settings)
 	g.GET("/xray", a.xraySettings)
+	g.GET("/navigation", a.navigation)
 
 	a.inboundController = NewInboundController(g)
 	a.settingController = NewSettingController(g)
@@ -46,4 +47,8 @@ func (a *XUIController) settings(c *gin.Context) {
 
 func (a *XUIController) xraySettings(c *gin.Context) {
 	html(c, "xray.html", "pages.xray.title", nil)
+}
+
+func (a *XUIController) navigation(c *gin.Context) {
+	html(c, "navigation.html", "pages.navigation.title", nil)
 }
